1b69033e82d1b6c270f3f43f812f48eaa585e63c
Aliendl3bar <aymanesalmoune00@gmail.com>
2026-01-02 09:49:02 +0100

    tst

---
+++ realisation.sql
@@ -0,0 +1,39 @@
+drop database if exists blog_rea_db; 
+CREATE DATABASE blog_rea_db;
+use blog_rea_db;
+
+CREATE TABLE user(
+id INT auto_increment primary key,
+name varchar(100) not null,
+email varchar(100) unique,
+password varchar(225) not null
+);
+create table category(
+id int auto_increment primary key,
+category_name varchar(100) not null
+);
+
+CREATE TABLE article(
+id INT auto_increment primary key,
+tittle varchar(225) not null,
+content text not null,
+status_public TINYINT(1) default 0,
+date_publication date,
+id_user INT,
+id_category INT,
+foreign key (id_user) references user(id),
+foreign key (id_category) references category(id)	
+);
+
+CREATE TABLE comment(
+id INT auto_increment primary key,
+content text not null,
+datep date,
+id_user INT,
+id_article INT,
+foreign key (id_user) references user(id),
+foreign key (id_article) references article(id)
+);
+
+create index namex on user (name);
+create index tittlex on article (tittle);
